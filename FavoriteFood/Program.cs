8ac304e391f0d57645617b05957f6090026727bd
Andrew Banks <riverbanks456@gmail.com>
2017-08-31 10:28:03 -0400

    2nd Commit

---
+++ FavoriteFood/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FavoriteFood
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What's your favorite food?: ");
+            String input = Console.ReadLine();
+            if (input == "pizza" || input == "Pizza")
+            {
+                Console.WriteLine("Hey, that's pretty good!");
+                Console.ReadLine();
+            }
+            else
+            {
+                Console.WriteLine("Wrong!  That is a terrible choice and you should feel bad about yourself.   Your favorite should be pizza!");
+                Console.ReadLine();
+            }
+        }
+    }
+}
