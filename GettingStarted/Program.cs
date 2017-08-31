46709d4a499465e3491670ac81bb17d074df8b5c
Andrew Banks <riverbanks456@gmail.com>
2017-08-31 10:06:27 -0400

    Initial Commit

---
+++ GettingStarted/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GettingStarted
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("jake paul is number 1");
+            Console.ReadLine();
+        }
+    }
+}
