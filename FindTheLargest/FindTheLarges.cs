36db96a6051944129ddc23f3743ce6ecda0a3b3c
ABanks28 <riverbanks456@gmail.com>
2017-09-06 11:26:59 -0400

    Started FindTheLargest
    title

---
+++ FindTheLargest/FindTheLarges.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FindTheLargest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Input the 1st number: ");
+            string firstNum = Console.ReadLine();
+            Console.WriteLine("Input the 2nd number: ");
+            string secondNum = Console.ReadLine();
+            Console.WriteLine("Input the 3rd number: ");
+            string thirdNum = Console.ReadLine();
+        }
+    }
+}
