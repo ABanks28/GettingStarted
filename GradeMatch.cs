40838359cb9ad160478725f2e44a3cb1b5647a92
ABanks28 <riverbanks456@gmail.com>
2017-09-08 11:28:47 -0400

    Palindrome

---
+++ GradeMatch.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Input the Grade: ");
+            string grade = Console.ReadLine();
+            
+            if (grade == "e")
+            {
+                Console.WriteLine("You have chosen: Excellent");
+                Console.ReadLine();
+            }
+            if (grade == "v")
+            {
+                Console.WriteLine("You have chosen: Very Good");
+                Console.ReadLine();
+            }
+            if (grade == "g")
+            {
+                Console.WriteLine("You have chosen: Good");
+                Console.ReadLine();
+            }
+            if (grade == "a")
+            {
+                Console.WriteLine("You have chosen: Average");
+                Console.ReadLine();
+            }
+            if (grade == "f")
+            {
+                Console.WriteLine("You have chosen: Fail");
+                Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ Palindrome/Palindrome.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Palindrome
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Type the palindrome: ");
+            string PalOrNah = Console.ReadLine();
+
+            PalOrNah = PalOrNah.Replace(" ", string.Empty);
+            string reverse = "";
+
+            for (int i = 0; i <= PalOrNah.Length; i++)
+            {
+                reverse = reverse + PalOrNah.Substring(PalOrNah.Length, PalOrNah.Length - i);
+            }
+            Console.WriteLine(reverse);
+            Console.ReadLine();
+        }
+    }
+}
