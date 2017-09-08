3221b2c73800cd2c4a99d992328cf4460d093e5f
ABanks28 <riverbanks456@gmail.com>
2017-09-08 10:49:36 -0400

    Finished FindTheLargest
    title

--- FindTheLargest/Program.cs
+++ FindTheLargest/Program.cs
@@ -10,6 +10,39 @@ namespace FindTheLargest
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Input the 1st number: ");
+            string firstNum = Console.ReadLine();
+            int f = Convert.ToInt32(firstNum);
+            Console.WriteLine("Input the 2nd number: ");
+            string secondNum = Console.ReadLine();
+            int s = Convert.ToInt32(secondNum);
+            Console.WriteLine("Input the 3rd number: ");
+            string thirdNum = Console.ReadLine();
+            int t = Convert.ToInt32(thirdNum);
+
+            if (f > s && f > t)
+            {
+                Console.WriteLine("The First Number is the Largest");
+                Console.ReadLine();
+            }
+            if (s > f && s > t)
+            {
+                Console.WriteLine("The Second Number is the Largest");
+                Console.ReadLine();
+            }
+            if (t > f && t > s)
+            {
+                Console.WriteLine("The Third Number is the Largest");
+                Console.ReadLine();
+            }
+            else
+            {
+                Console.WriteLine("There is no largest number.");
+                Console.ReadLine();
+            }
+
+
+
         }
     }
 }
