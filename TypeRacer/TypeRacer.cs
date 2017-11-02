e7ad93515813b5e59e761354ca8ea9bc30f6e72f
ABanks28 <riverbanks456@gmail.com>
2017-11-02 11:28:51 -0400

    TypeRacer

---
+++ TypeRacer/TypeRacer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TypeRacer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var watch = System.Diagnostics.Stopwatch.StartNew();
+            // the code that you want to measure comes here
+            watch.Stop();
+            var elapsedMs = watch.ElapsedMilliseconds;
+        }
+    }
+}
